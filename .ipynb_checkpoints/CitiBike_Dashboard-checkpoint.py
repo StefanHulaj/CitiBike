d7fcf046515a00008b41598a7bd0a0d60c35b015
Stefan Hulaj <HulajStefan@gmail.com>
2025-11-18 12:09:34 -0800

    2.6

---
+++ .ipynb_checkpoints/CitiBike_Dashboard-checkpoint.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from streamlit_keplergl import keplergl_static 
+from keplergl import KeplerGl
+from datetime import datetime as dt 
+import os
+
+####
+
+path = r"C:\Users\stefa\CitiBike"
+
+####
+
+df = pd.read_csv(os.path.join(path, 'Prepared Data', 'CitiBikeWeatherWrangled.csv'),  index_col = False)
+
+####
+
+st.set_page_config(page_title = 'Citi Bike Strategy Dashboard', layout='wide')
+
+####
+
+st.title("Citi Bike Strategy Dashboard")
+
+####
+
+st.markdown("The dashboard will help visualize current usage trends of Citi Bike")
+
+# ########################### DEFINE THE CHARTS ############################
+
+
+## Bar chart 
+
+## Groupby
+
+df['value'] = 1 
+df_groupby_bar = df.groupby('start_station_name', as_index=False).agg({'value': 'sum'})
+top20 = df_groupby_bar.nlargest(20, 'value')
+
+fig = go.Figure(go.Bar(x = top20['start_station_name'], y = top20['value'], marker={'color': top20['value'],'colorscale': 'Blues'}))
+
+fig.update_layout(
+    title = 'Top 20 Most Popular Bike Stations in New York City',
+    xaxis_title = 'Start Stations', 
+    yaxis_title = 'Sum of Trips', 
+    width = 900, height = 600
+)
+
+st.plotly_chart(fig, width=True)
+
+####
+
+df_1 = df.drop(columns = {'started_at', 'ended_at', 'ride_id', 'start_station_id', 'end_station_id','_merge'})
+
+####
+
+## line chart 
+
+fig_2 = make_subplots(specs=[[{"secondary_y": True}]])
+
+fig_2.add_trace(
+go.Scatter(x = df_1['date'], y = df_1['bike_rides_daily'], name = 'Daily bike rides', marker={'color': 'blue'}), secondary_y = False)
+
+fig_2.add_trace(
+go.Scatter(x=df_1['date'], y=df_1['average_tempurature'], name='Daily temperature', marker={'color': 'red'}), secondary_y=True)
+
+st.header("Daily Bike Trips and Tempurature in New York City")
+st.plotly_chart(fig_2, width=True)
+
+####
+
+### Add the map  ###
+
+path_to_html = "CitiBike Bike Trips Aggregated.html"
+
+# Read file and keep in variable 
+with open(path_to_html, 'r') as f:
+    html_data = f.read()
+
+## Show in web page 
+st.header("Aggregated Bike Trips in New York City")
+st.components.v1.html(html_data,height = 1000)
+
